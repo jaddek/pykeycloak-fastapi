b74a1f7d71732a9cf8a610f18483e7404aa291da
tonynazarov <tonynazarov@gmail.com>
2026-01-20 23:30:51 +0300

    Test build

--- src/pykeycloak_fastapi/aliases.py
+++ src/pykeycloak_fastapi/aliases.py
@@ -1,40 +1,39 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2026 Anton "Tony" Nazarov <tonynazarov+dev@gmail.com>
 
-from typing import Annotated, TypeAlias
+from typing import Annotated
 
-from fastapi.params import Depends
+from fastapi import Depends
 from pykeycloak.providers.providers import KeycloakInMemoryProviderAsync
-from pykeycloak.services.services import UmaService, AuthService, RolesService, SessionsService, UsersService
+from pykeycloak.services.services import (
+    AuthService,
+    RolesService,
+    SessionsService,
+    UmaService,
+    UsersService,
+)
+
 from .dependencies import (
     get_in_memory_keycloak_provider,
-    get_keycloak_uma_service,
     get_keycloak_auth_service,
     get_keycloak_roles_service,
     get_keycloak_sessions_service,
-    get_keycloak_users_service
+    get_keycloak_uma_service,
+    get_keycloak_users_service,
 )
 
-KeycloakInMemoryProviderAsyncAlias: TypeAlias = Annotated[
+type KeycloakInMemoryProvider = Annotated[
     KeycloakInMemoryProviderAsync, Depends(get_in_memory_keycloak_provider)
 ]
 
-KeycloakUmaServiceAlias: TypeAlias = Annotated[
-    UmaService, Depends(get_keycloak_uma_service)
-]
+type KeycloakUmaService = Annotated[UmaService, Depends(get_keycloak_uma_service)]
 
-KeycloakAuthServiceAlias: TypeAlias = Annotated[
-    AuthService, Depends(get_keycloak_auth_service)
-]
+type KeycloakAuthService = Annotated[AuthService, Depends(get_keycloak_auth_service)]
 
-KeycloakRolesServiceAlias: TypeAlias = Annotated[
-    RolesService, Depends(get_keycloak_roles_service)
-]
+type KeycloakRolesService = Annotated[RolesService, Depends(get_keycloak_roles_service)]
 
-KeycloakSessionsServiceAlias: TypeAlias = Annotated[
+type KeycloakSessionsService = Annotated[
     SessionsService, Depends(get_keycloak_sessions_service)
 ]
 
-KeycloakUsersServiceAlias: TypeAlias = Annotated[
-    UsersService, Depends(get_keycloak_users_service)
-]
+type KeycloakUsersService = Annotated[UsersService, Depends(get_keycloak_users_service)]
--- src/pykeycloak_fastapi/dependencies.py
+++ src/pykeycloak_fastapi/dependencies.py
@@ -6,36 +6,52 @@ from functools import lru_cache
 from fastapi import Depends
 from pykeycloak.core.realm import RealmClient
 from pykeycloak.providers.providers import KeycloakInMemoryProviderAsync
-from pykeycloak.services.services import UmaService, AuthService, RolesService, UsersService, SessionsService
+from pykeycloak.services.services import (
+    AuthService,
+    RolesService,
+    SessionsService,
+    UmaService,
+    UsersService,
+)
 
 
 @lru_cache(1)
 def get_in_memory_keycloak_provider() -> KeycloakInMemoryProviderAsync:
-
     realm_client = RealmClient.from_env()
 
-    return KeycloakInMemoryProviderAsync(
-        realm="realm",
-        realm_client=realm_client
-    )
+    return KeycloakInMemoryProviderAsync(realm="realm", realm_client=realm_client)
+
 
 @lru_cache(1)
-def get_keycloak_uma_service(provider=Depends(get_in_memory_keycloak_provider)):
-    return  UmaService(provider=provider)
+def get_keycloak_uma_service(
+    provider: KeycloakInMemoryProviderAsync = Depends(get_in_memory_keycloak_provider),
+) -> UmaService:
+    return UmaService(provider=provider)
+
 
 @lru_cache(1)
-def get_keycloak_auth_service(provider=Depends(get_in_memory_keycloak_provider)):
-    return  AuthService(provider=provider)
+def get_keycloak_auth_service(
+    provider: KeycloakInMemoryProviderAsync = Depends(get_in_memory_keycloak_provider),
+) -> AuthService:
+    return AuthService(provider=provider)
+
 
 @lru_cache(1)
-def get_keycloak_roles_service(provider=Depends(get_in_memory_keycloak_provider)):
-    return  RolesService(provider=provider)
+def get_keycloak_roles_service(
+    provider: KeycloakInMemoryProviderAsync = Depends(get_in_memory_keycloak_provider),
+) -> RolesService:
+    return RolesService(provider=provider)
+
 
 @lru_cache(1)
-def get_keycloak_sessions_service(provider=Depends(get_in_memory_keycloak_provider)):
-    return  SessionsService(provider=provider)
+def get_keycloak_sessions_service(
+    provider: KeycloakInMemoryProviderAsync = Depends(get_in_memory_keycloak_provider),
+) -> SessionsService:
+    return SessionsService(provider=provider)
+
 
 @lru_cache(1)
-def get_keycloak_users_service(provider=Depends(get_in_memory_keycloak_provider)):
-    return  UsersService(provider=provider)
-
+def get_keycloak_users_service(
+    provider: KeycloakInMemoryProviderAsync = Depends(get_in_memory_keycloak_provider),
+) -> UsersService:
+    return UsersService(provider=provider)
